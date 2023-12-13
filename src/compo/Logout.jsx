fa3cccee7b8ad071f6ff453b711cfa8989188fa8
manishp3 <patadiyamanish20@gmail.com>
2023-12-14 01:11:15 +0530

    Initial commit

---
+++ src/compo/Logout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import authService from '../appwrite/auth'
+import { logout } from '../store/authSlice'
+
+
+function Logout() {
+    const dispatch=useDispatch()
+    const logoutHandler=()=>{
+        authService.logout().then(()=>{
+            dispatch(logout())
+        })
+    }
+  return (
+    <button onClick={logoutHandler}>Logout</button>
+  )
+}
+
+export default Logout
---
+++ src/compo/index.js
@@ -0,0 +1,32 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Logout from "./Logout";
+import Container from "./container";
+import Logo from "./Logo";
+import Button from "./Button";
+import RTE from './RTE'
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./PostForm";
+import Input from './Input'
+import PostCard from './PostCard'
+import AuthLayout from './AuthLayout'
+import Select from './Select'
+
+export {
+
+    Header,
+    Footer,
+    Container,
+    Logo,
+    Logout,
+    Button,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout,
+    Input,
+    Select
+}
